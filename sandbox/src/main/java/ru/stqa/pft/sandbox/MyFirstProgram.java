92d9f794c652885d7ba72db03cd199a89eb82f20
Tetyana Kostyrkina <tatiys2006@gmail.com>
2016-04-18 11:47:16 +0300

    Lesson 2 task 3 done

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,38 +3,21 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	
 	public static void main(String[] args) {
-		hello("world");
+        hello("world");
       hello("user");
 
       Square s = new Square(5);
 
-      System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+      System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
       Rectangle r = new Rectangle(4,6);
 
-      System.out.println("Площадь квадрата со сторонами " + r.a + " и " + r.b + " = " + area(r));
-
-      Point p = new Point(4,5,6,7);
-      System.out.println("Distance between " + p.x1 + "." + p.y1 + " and " + p.x2 + "." + p.y2 + "are" + distance(p));
-
-  }
+      System.out.println("Площадь квадрата со сторонами " + r.a + " и " + r.b + " = " + r.area());
+     }
 
     public static void hello(String somebody) {
 
         System.out.println("Hello, " + somebody + "!");
-    }
-
-    public static double area(Square s){
-        return s.l * s.l;
-    }
-
-    public static double area(Rectangle r) {
-        return r.a * r.b;
-    }
-
-    public static double distance(Point p1, Point p2){
-        return Math.sqrt(p.p1 + p.p2);
-        //d = \/(х2— х1)2 + (y2— y1)2
     }
 }
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,19 +2,31 @@ package ru.stqa.pft.sandbox;
 
 /**
  * Created by tetyana.kostyrkina on 4/13/2016.
- */
+  */
 public class Point {
     public double x1;
     public double y1;
-
     public double x2;
     public double y2;
 
-    public double p1;
-    public double p2;
 
-    public Point(double p1, double p2) {
-        this.p1 = (x2 - x1)*(x2 - x1);
-        this.p2 = (y2 - y1)*(y2 - y1);
+    public static void main(String[] args) {
+       
+        Point p = new Point(1.0, 1, 2, 1.0);
+
+        System.out.println("Distance between " + p.x1 + "," + p.y1 + " and " + p.x2 + "," + p.y2 + " are " +
+                p.distance());
+        
+       }
+    public Point(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
-}
+    public double distance(){
+        return Math.sqrt((this.x2 - this.x1)*(this.x2 - this.x1) + (this.y2 - this.y1)*(this.y2 - this.y1));
+    }
+  }
+
+
