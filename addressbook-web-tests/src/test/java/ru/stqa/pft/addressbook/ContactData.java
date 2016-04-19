1d19b218edd839a2003706d8afb1db0e58e7f102
Tetyana Kostyrkina <tatiys2006@gmail.com>
2016-04-19 15:17:14 +0300

    Lesson 2 hometask 4 done

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String lastName;
+    private final String address;
+    private final String phoneNumberHome;
+    private final String email;
+
+    public ContactData(String firstName, String lastName, String address, String phoneNumberHome, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumberHome = phoneNumberHome;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumberHome() {
+        return phoneNumberHome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
