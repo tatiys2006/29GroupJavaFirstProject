31c8292f54ec2a872bf5adda9cfa792d89541ed1
Tetyana Kostyrkina <tatiys2006@gmail.com>
2016-04-19 16:47:05 +0300

    Lesson 2 part 10 Created Helpers

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by tetyana.kostyrkina on 4/19/2016.
+ */
+public class NavigationHelper {
+    private  FirefoxDriver wd;
+
+    public NavigationHelper(FirefoxDriver wd) {
+        this.wd = wd;
+    }
+
+    public void gotoGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -8,11 +8,11 @@ public class GroupCreationTest extends TestBase {
     @Test
     public void testGroupCreation() {
 
-        app.gotoGroupPage();
-        app.initGroupCreation();
-        app.fillGroupForm(new GroupData("GroupName1", "Group Header1", "Group Footer1"));
-        app.submitGroupCreation();
-        app.returnToGroupPage();
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData("GroupName1", "Group Header1", "Group Footer1"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
     }
 
 }
