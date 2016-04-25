022bb17ab0e6a615896f71a5696143fcb00b4d27
Tetyana Kostyrkina <tatiys2006@gmail.com>
2016-04-25 15:45:58 +0300

    Lesson 3 part 8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -10,7 +10,7 @@ public class ContactCreationTest extends TestBase {
     public void testContactCreation() {
 
         app.getContactHelper().initContactCreation();
-        app.getContactHelper().fillContactData(new ContactData("FirstName", null, null, "111222333", "firstname.lastname@test.com"));
+        app.getContactHelper().fillContactData(new ContactData("FirstName", null, null, "111222333", "firstname.lastname@test.com", "GroupName1"), true);
         //app.getContactHelper().fillContactData(new ContactData("FirstName", "LastName", "Myaddress", "111222333", "firstname.lastname@test.com"));
         app.getContactHelper().submitContactData();
         app.getContactHelper().gotoHomePage();
