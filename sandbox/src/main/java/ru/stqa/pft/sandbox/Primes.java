5415fb784ee97c784e81825653464d2624a6278e
Tetyana Kostyrkina <tatiys2006@gmail.com>
2016-04-29 13:44:13 +0300

    Lecture 4 part 1

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Primes.java
@@ -0,0 +1,45 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by tetyana.kostyrkina on 4/29/2016.
+ */
+public class Primes {
+
+    public static boolean isPrime(int n){
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPrimeFast(int n){
+        int m = (int) Math.sqrt(n); // preibrazovanie v celoe 4islo
+        for (int i = 2; i < m; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static boolean isPrimeWhile(int n){
+        int i = 2;
+        while (i < n && n % i != 0){
+           i++;
+        }
+        return i == n;
+    }
+
+    public static boolean isPrime(long n){
+        for (long i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
