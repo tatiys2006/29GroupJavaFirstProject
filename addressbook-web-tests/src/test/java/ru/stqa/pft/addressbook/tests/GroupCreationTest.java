70dae5ae1730a101cc69bca304ce5a497f0771c1
Tetyana Kostyrkina <tatiys2006@gmail.com>
2016-05-04 14:36:23 +0300

    Lecture 4 part 9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -15,20 +15,21 @@ public class GroupCreationTest extends TestBase {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
         // int before = app.getGroupHelper().getGroupCount();
-        GroupData group = new GroupData("GroupName1", null, null);
+        GroupData group = new GroupData("GroupName5", null, null);
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         // int after = app.getGroupHelper().getGroupCount();
         Assert.assertEquals(after.size(), before.size() + 1);
         // Assert.assertEquals(after, before + 1);
 
-        int max = 0;
+       /* int max = 0;
         for (GroupData g : after) {
             if (g.getId() > max) {
                 max = g.getId();
             }
         }
-        group.setId(max);
+        */
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
